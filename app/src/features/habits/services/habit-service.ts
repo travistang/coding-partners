93dd107c57443ff155542762d299b16698e57d3a
Travis Tang <travis.tang@tum.de>
2025-04-18 18:02:31 +0200

    :rocket: Deploy to vercel

--- app/src/features/habits/services/habit-service.ts
+++ app/src/features/habits/services/habit-service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Habit, HabitWithCompletion } from '../types';
 
-const BASE_URL = 'http://localhost:3000/api/habits';
+const BASE_URL = import.meta.env.VITE_API_URL || '/api/habits';
 
 
 export const HabitService = {
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,19 +2,11 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   build: {
     outDir: 'dist',
     emptyOutDir: true
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  }
 })
